ae76f1021be8fcc54ba4baf4b7586c0c82f2146a
Scray, Lance <lrscray@ilstu.edu>
2020-03-27 23:30:54 -0500

    commiting optimized project

---
+++ Rampage/Assets/Scripts/Character Scripts/ObjectPool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    //Use a pooler to optimize object spwaning
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int instances;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+        //Create dictionary to store new instances game object in queue
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            //store exactly number of instances specified
+            for (int i = 0; i < pool.instances; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+}
--- Rampage/Assets/Scripts/Character Scripts/Spawn.cs
+++ Rampage/Assets/Scripts/Character Scripts/Spawn.cs
@@ -5,35 +5,12 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     //Spawn enemies randomly within given parameters
-    [SerializeField] GameObject Enemy;
-    [SerializeField] float xMin = -10f;
-    [SerializeField] float xMax = 10f;
-    [SerializeField] float zMin = -10f;
-    [SerializeField] float zMax = 10f;
-    [SerializeField] float initialHeight = 1f;
+    [SerializeField] private GameObject Enemy;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey("space"))
-        {
-            SpawnEnemy();
-        }
-        if (Input.GetKeyDown("escape"))
-        {
-            Application.Quit();
-        }
+        Instantiate(Enemy, transform.position, Quaternion.identity);
     }
 
-    void SpawnEnemy()
-    { //have enemies fire out of their spawn points
-        Vector3 Position = new Vector3(Random.Range(xMin, xMax), initialHeight, Random.Range(zMin, zMax));
-        GameObject BadGuy = Instantiate(Enemy);
-        BadGuy.transform.position = Position;
-        Rigidbody rb = BadGuy.GetComponent<Rigidbody>();
-        rb.AddTorque(new Vector3(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5)));
-        BadGuy.SetActive(true);
-        rb.AddForce(transform.forward * 1000, ForceMode.Acceleration);
-        rb.useGravity = true;
-    }
 }
